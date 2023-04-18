e6a5a4771ed9e88781215a4f6313a648e6802140
russellbanares322 <banaresrussell11@gmail.com>
2023-04-18 21:00:24 +0800

    feat: add validation in email

--- src/components/patient-form/FormModal.jsx
+++ src/components/patient-form/FormModal.jsx
@@ -1,8 +1,9 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./styles.module.css";
 
 const FormModal = ({ handleCloseModal, inputs, handleChangeFields }) => {
   const [input, setInput] = useState("");
+  const [error, setError] = useState("");
   const handleSubmit = (e) => {
     e.preventDefault();
     handleChangeFields({ firstname: inputs.firstname });
@@ -19,15 +20,59 @@ const FormModal = ({ handleCloseModal, inputs, handleChangeFields }) => {
       e.preventDefault();
 
       let email = e.target.value.trim();
-      if (email) {
+      if (email && isValid(input)) {
         handleChangeFields({
           email: [...inputs.email, email],
         });
         setInput("");
       }
     }
+    if (inputs.email.includes(input)) {
+      return alert(`${input} is already added, try a new one!`);
+    }
+  };
+  const isValid = (value) => {
+    let errorMessage = null;
+
+    if (!isEmail(value)) {
+      errorMessage = `${value} is not a valid email address.`;
+    }
+    if (inList(value)) {
+      errorMessage = `${value} has already been added.`;
+    }
+    if (errorMessage) {
+      setError(errorMessage);
+      return false;
+    }
+    return true;
   };
 
+  const isEmail = (value) => {
+    return /[\w\d\.-]+@[\w\d\.-]+\.[\w\d\.-]+/.test(value);
+  };
+  const inList = (value) => {
+    return inputs.email.includes(value);
+  };
+
+  const handlePaste = (e) => {
+    e.preventDefault();
+
+    let paste = e.clipboardData.getData("text");
+    let emails = paste.match(/[\w\d\.-]+@[\w\d\.-]+\.[\w\d\.-]+/g);
+
+    if (emails) {
+      let toBeAdded = emails.filter((email) => inList(email));
+
+      handleChangeFields({
+        email: [...inputs.email, ...toBeAdded],
+      });
+    }
+  };
+
+  const handleChange = (e) => {
+    setInput(e.target.value);
+    setError("");
+  };
   const handleRemove = (selectedEmail) => {
     const newEmails = inputs.email.filter((m) => m !== selectedEmail);
     handleChangeFields({ email: newEmails });
@@ -71,8 +116,9 @@ const FormModal = ({ handleCloseModal, inputs, handleChangeFields }) => {
           ))}
           <input
             value={input}
-            onChange={(e) => setInput(e.target.value)}
+            onChange={handleChange}
             onKeyDown={(e) => handleEnterEmail(e)}
+            onPaste={handlePaste}
             type="email"
           />
           {input.includes("@") && input.length > 6 && (
@@ -80,6 +126,7 @@ const FormModal = ({ handleCloseModal, inputs, handleChangeFields }) => {
               {input}
             </p>
           )}
+          {error && <p style={{ color: "red" }}>{error}</p>}
         </div>
       </div>
     </div>
