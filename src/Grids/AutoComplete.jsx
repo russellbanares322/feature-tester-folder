df65550f05889fc887b47fb7463ee30fd8ac4fe5
russellbanares322 <banaresrussell11@gmail.com>
2023-05-05 21:24:15 +0800

    feat: implement editing of data inside the mini table

--- src/Grids/AutoComplete.jsx
+++ src/Grids/AutoComplete.jsx
@@ -1,15 +1,34 @@
 import React, { useState } from "react";
-import { Autocomplete, Box, TextField, Typography } from "@mui/material";
-const top100Films = [
-  { code: "testing A" },
-  { code: "testing B" },
-  { code: "testing C" },
-  { code: "testing D" },
+import {
+  Autocomplete,
+  Box,
+  Button,
+  TextField,
+  Typography,
+} from "@mui/material";
+import Dboard from "./Dboard";
+const codeOptions = [
+  { id: 1, code: "testing A" },
+  { id: 2, code: "testing B" },
+  { id: 3, code: "testing C" },
+  { id: 4, code: "testing D" },
 ];
 
 const AutoComplete = () => {
+  const [step, setStep] = useState(1);
+
+  const handleNext = (e) => {
+    e.preventDefault();
+    setStep((prevStep) => Math.min(2, prevStep + 1));
+  };
+
+  const handlePrev = (e) => {
+    e.preventDefault();
+    setStep((prevStep) => Math.max(1, prevStep - 1));
+  };
   const [movieYear, setMovieYear] = useState(null);
   const [codeInputs, setCodeInputs] = useState({
+    id: Math.random(),
     code: "",
   });
   const [codesArr, setCodesArr] = useState([]);
@@ -18,28 +37,40 @@ const AutoComplete = () => {
     setCodesArr([...codesArr, selectedCode]);
   };
 
+  console.log(selectedCodeId);
   const handleEnterCode = (e) => {
-    if (e.key === "Enter") {
-      setCodesArr([...codesArr, codeInputs]);
+    if (e.key === "Enter" && selectedCodeId === null) {
+      setCodesArr([...codesArr, { id: Math.random(), code: codeInputs.code }]);
       setCodeInputs({
+        id: 0,
+        code: "",
+      });
+    }
+
+    if (e.key === "Enter" && selectedCodeId !== null) {
+      setCodesArr((prevCodesArr) =>
+        prevCodesArr.map((c) =>
+          c.id === selectedCodeId ? { ...c, ...codeInputs } : c
+        )
+      );
+      setSelectedCodeId(null);
+      setCodeInputs({
+        id: 0,
         code: "",
       });
     }
   };
 
-  const handleSelectCodeToEdit = (selectedCode, selectedCodeIdx) => {
-    if (!top100Films.includes(selectedCode)) setCodeInputs({ ...selectedCode });
-    setSelectedCodeId(selectedCodeIdx);
+  const handleSelectCodeToEdit = (selectedCode) => {
+    if (!codeOptions.includes(selectedCode)) {
+      setCodeInputs({ ...selectedCode });
+      setSelectedCodeId(selectedCode.id);
+    }
   };
 
-  const handleSave = () => {
-    setCodesArr((prevCodesArr) =>
-      prevCodesArr.map((c, idx) =>
-        idx === selectedCodeId ? { ...c, ...codeInputs } : c
-      )
-    );
-
-    setSelectedCodeId(null);
+  const handleChange = (e) => {
+    setCodeInputs({ code: e.target.value });
+    console.log(codeInputs.code);
   };
   return (
     <div
@@ -53,67 +84,93 @@ const AutoComplete = () => {
         flexDirection: "column",
       }}
     >
-      <h1 color="white">{movieYear}</h1>
-      <Autocomplete
-        disableClearable
-        getOptionLabel={(option) => (option.code ? `${option.code}` : "")}
-        onChange={(event, newInputValue) => {
-          handleSelectCode(newInputValue);
-        }}
-        id="combo-box-demo"
-        options={top100Films}
-        sx={{ width: 300, backgroundColor: "white" }}
-        renderInput={(params) => (
-          <TextField {...params} placeholder="Province" />
-        )}
-      />
-      <TextField
-        id="outlined-multiline-flexible"
-        label="Write here"
-        multiline
-        maxRows={5}
-        value={codeInputs.code}
-        onKeyDown={(e) =>
-          selectedCodeId === null ? handleEnterCode(e) : handleSave()
-        }
-        onChange={(e) => setCodeInputs({ code: e.target.value })}
-      />
-      <Box>
-        <Box
-          sx={{
-            display: "grid",
-            gridTemplateColumns: "9fr 30fr",
-          }}
-        >
-          <Typography variant="p">No</Typography>
-          <Typography variant="p">Code</Typography>
+      <h1 style={{ color: "red" }}>{step}</h1>
+
+      <Button
+        type="button"
+        onClick={handleNext}
+        variant="contained"
+        color="primary"
+      >
+        Next
+      </Button>
+      <Button
+        type="button"
+        onClick={handlePrev}
+        variant="contained"
+        color="primary"
+      >
+        Prev
+      </Button>
+      {JSON.stringify(codesArr)}
+      {step === 2 && (
+        <Box>
+          <h1 color="white">{movieYear}</h1>
+          <Autocomplete
+            disableClearable
+            getOptionLabel={(option) =>
+              option.id && option.code ? `${option.code}` : ""
+            }
+            onChange={(event, newInputValue) => {
+              handleSelectCode(newInputValue);
+            }}
+            id="combo-box-demo"
+            options={codeOptions}
+            sx={{ width: 300, backgroundColor: "white" }}
+            renderInput={(params) => (
+              <TextField {...params} placeholder="Province" />
+            )}
+          />
+          <TextField
+            label="Write here"
+            multiline
+            rows={2}
+            name="code"
+            onKeyDown={(e) => handleEnterCode(e)}
+            onChange={handleChange}
+            value={codeInputs.code}
+            type="text"
+            variant="outlined"
+          />
+          <Box>
+            <Box
+              sx={{
+                display: "grid",
+                gridTemplateColumns: "9fr 30fr",
+              }}
+            >
+              <Typography variant="p">No</Typography>
+              <Typography variant="p">Code</Typography>
+            </Box>
+            <Box
+              sx={{
+                border: "1px solid black",
+                borderRadius: "0.5rem",
+                padding: "0.2rem",
+                height: "auto",
+                paddingBottom: "2rem",
+                width: "9rem",
+              }}
+            >
+              {codesArr &&
+                codesArr.map((a, index) => (
+                  <Box
+                    sx={{
+                      display: "grid",
+                      gridTemplateColumns: "9fr 30fr",
+                    }}
+                    key={a.id}
+                    onDoubleClick={() => handleSelectCodeToEdit(a)}
+                  >
+                    <Typography variant="p">{index + 1}</Typography>
+                    <Typography variant="p">{a.code}</Typography>
+                  </Box>
+                ))}
+            </Box>
+          </Box>
         </Box>
-        <Box
-          sx={{
-            border: "1px solid black",
-            borderRadius: "0.5rem",
-            padding: "0.2rem",
-            height: "auto",
-            paddingBottom: "2rem",
-            width: "9rem",
-          }}
-        >
-          {codesArr &&
-            codesArr.map((a, index) => (
-              <Box
-                sx={{
-                  display: "grid",
-                  gridTemplateColumns: "9fr 30fr",
-                }}
-                key={a.no}
-                onDoubleClick={() => handleSelectCodeToEdit(a, index)}
-              >
-                <Typography variant="p">{index + 1}</Typography>
-                <Typography variant="p">{a.code}</Typography>
-              </Box>
-            ))}
-        </Box>
-      </Box>
+      )}
+      {step === 1 && <Dboard />}
     </div>
   );
 };
--- src/Grids/fStep.jsx
+++ src/Grids/fStep.jsx
@@ -1,15 +1,14 @@
-import React from "react";
-import { Box } from "@mui/material";
+import React, { useState } from "react";
+import { Box, Button } from "@mui/material";
 import FirstSection from "./firstSection";
 import SecondSection from "./secondSection";
+import AutoComplete from "./Autocomplete";
+import Dboard from "./Dboard";
 
 const FStep = () => {
-  return (
-    <Box sx={{ flexGrow: 1 }} p={4}>
-      <FirstSection />
-      <SecondSection />
-    </Box>
-  );
+  const [step, setStep] = useState(1);
+
+  return <Box sx={{ flexGrow: 1 }} p={4}></Box>;
 };
 
 export default FStep;
